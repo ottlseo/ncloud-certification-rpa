c55f4b05852c47a0b4dcc41dcc0351e9e9e57428
ottl-seo <dotsi@ewhain.net>
2022-11-10 11:45:15 +0900

    [test] excel 셀 붙여넣기 테스트

--- driver.py
+++ driver.py
@@ -1,14 +1,16 @@
 
-import openpyexcel
+import openpyxl
 
 # step 1. 메일 전송용 엑셀 만들기
-wb_mail = openpyexcel.Workbook()
+wb_mail = openpyxl.Workbook()
 mailsheet = wb_mail.active
 mailsheet.append(["수신자 Email 주소", "date", "link"])  # <--맨 첫 행에 추가
 
 # 수험자 리스트 엑셀 열기 (C열-> L열로 복사 / A열-> M열로 복사 / N열에 크롤링 정보 넣을 예정)
-
-wb = openpyexcel.load_workbook("Book.xlsx")
+wb = openpyxl.load_workbook("./data/자격시험수험자리스트_1101.xlsx")
+originsheet = wb.active
+old_cell = originsheet.cell(1,1)
+new_cell = mailsheet.cell(2,1, value=old_cell.value)
 
 # step 2. 엑셀 중복값 표시
 # 나중에
@@ -18,4 +20,4 @@ wb = openpyexcel.load_workbook("Book.xlsx")
 # step 4. 엑셀에 정보 붙여넣기
 #sheet.append([title, genre, audience])
 
-wb.save("MASS_INPUT_FORM_today.xlsx")
+wb_mail.save("./result/MASS_INPUT_FORM_today.xlsx")

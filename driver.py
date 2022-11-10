69cee72fe91cbedb915f7184230988c0bd89a609
ottl-seo <dotsi@ewhain.net>
2022-11-10 15:14:37 +0900

    [#2] 이메일 붙여넣기 완료

--- driver.py
+++ driver.py
@@ -1,35 +1,32 @@
 
 import openpyxl
 
-# step 1. 메일 전송용 엑셀 만들기
-wb_mail = openpyxl.Workbook()
-mailsheet = wb_mail.active
-mailsheet.append(["수신자 Email 주소", "date", "link"])  # <--맨 첫 행에 추가
-
-# 수험자 리스트 엑셀 열기 (C열-> L열로 복사 / A열-> M열로 복사 / N열에 크롤링 정보 넣을 예정)
-wb = openpyxl.load_workbook("./data/자격시험수험자리스트_1101.xlsx")
+# 수험자 리스트 엑셀 열기  // N열에 크롤링 정보 넣을 예정)
+wb = openpyxl.load_workbook("./data/자격시험수험자리스트_1102.xlsx")
 originsheet = wb.active
-old_cell = originsheet.cell(1,1)
-new_cell = mailsheet.cell(2,1, value=old_cell.value)
 
-# mailsheet[2:10,1] = originsheet[2:10,3] # 안 됨
+MAX_ROW = 0
 
-""" 
-#이렇게 해야 함
-allList = []
-for row in sheet.iter_rows(min_row=1, max_row=10, min_col=2, max_col=5):
-    a = []
-    for cell in row:
-        a.append(cell.value)
-    allList.append(a)
-"""
+# 이메일 셀 범위 복사 붙여넣기 (C열-> L열로 복사)
+columns = originsheet.iter_cols(min_col=3, max_col=3)
+for col in columns:
+    for cell in col:
+        cell_new = originsheet.cell(row=cell.row, column=12, value=cell.value)
+        MAX_ROW += 1  # 총 row 개수는 (MAX_ROW-1) 개
 
-# step 2. 엑셀 중복값 표시
-# 나중에
+# step 2. 엑셀 중복값 표시 -> 나중에
 
 # step 3. Selenium으로 브라우저 원격 제어
 
 # step 4. 엑셀에 정보 붙여넣기
 #sheet.append([title, genre, audience])
 
+#wb.save("./result/temp_1102.xlsx")
+
+"""
+# 나중에) 메일 전송용 엑셀 만들기
+wb_mail = openpyxl.Workbook()
+mailsheet = wb_mail.active
+mailsheet.append(["수신자 Email 주소", "date", "link"])  # <--맨 첫 행에 추가
 wb_mail.save("./result/MASS_INPUT_FORM_today.xlsx")
+"""

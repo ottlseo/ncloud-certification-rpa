73d2ac154d40653eaa9c55c64f0a2961ea042f47
ottl-seo <dotsi@ewhain.net>
2022-11-13 03:19:09 +0900

    [#15] 엑셀 준비 코드 함수분리

--- driver.py
+++ driver.py
@@ -7,34 +7,40 @@ from selenium.webdriver.support import expected_conditions as EC
 import undetected_chromedriver as uc
 import accountInfo
 
+
 ###### [STEP 1] 수험자 리스트 엑셀 열기 ######
 wb = openpyxl.load_workbook("./data/자격시험수험자리스트_1102.xlsx")
 originsheet = wb.active
 
-#MAX_ROW = -1
-MAX_ROW = 10
+
 """
 ###### [STEP 2] 기본 정보 붙여넣기 ######
-# 이메일 셀 범위 복사 붙여넣기 (C열-> L열로 복사)
-columns = originsheet.iter_cols(min_col=3, max_col=3)
-for col in columns:
-    for cell in col:
-        cell_new = originsheet.cell(row=cell.row, column=12, value=cell.value)
-        MAX_ROW += 1  # 총 row 개수는 (MAX_ROW-1) 개
-
-# 참조용 메일 주소 넣기
-originsheet.cell(row=MAX_ROW+2, column=12).value = "dl_edu_cert@navercorp.com"
-originsheet.cell(row=MAX_ROW+3, column=12).value = "dl_certification@navercorp.com"
-
-# 날짜 삽입 (A열-> M열)
-dateValue = (originsheet.cell(row=2, column=1).value) % 10000  ## 1102
-month = (int)(dateValue / 100)
-day = (dateValue % 100)
-dateString = str(month)+"월 "+str(day)+"일"  # 월-일 형태의 스트링으로 변환
-for i in range(0,MAX_ROW+2):
-    originsheet.cell(row=i+2, column=13).value = dateString  # M열에 삽입
 """
 if __name__=="__main__":
+    def prepare_form(sheet):
+        global MAX_ROW
+        MAX_ROW = -1
+
+        # 이메일 셀 범위 복사 붙여넣기 (C열-> L열로 복사)
+        columns = originsheet.iter_cols(min_col=3, max_col=3)
+        for col in columns:
+            for cell in col:
+                cell_new = originsheet.cell(row=cell.row, column=12, value=cell.value)
+                MAX_ROW += 1  # 총 row 개수는 (MAX_ROW-1) 개
+
+        # 참조용 메일 주소 넣기
+        originsheet.cell(row=MAX_ROW + 2, column=12).value = "dl_edu_cert@navercorp.com"
+        originsheet.cell(row=MAX_ROW + 3, column=12).value = "dl_certification@navercorp.com"
+
+        # 날짜 삽입 (A열-> M열)
+        dateValue = (originsheet.cell(row=2, column=1).value) % 10000  ## 1102
+        month = (int)(dateValue / 100)
+        day = (dateValue % 100)
+        dateString = str(month) + "월 " + str(day) + "일"  # 월-일 형태의 스트링으로 변환
+        for i in range(0, MAX_ROW + 2):
+            originsheet.cell(row=i + 2, column=13).value = dateString  # M열에 삽입
+        print("--FORM GENERATED--")
+
     def login(driver):  ### 초기 1회 ###
         # '로그인' 클릭
         driver.find_element(By.CSS_SELECTOR, "a.glue-header__link").click()
@@ -49,8 +55,8 @@ if __name__=="__main__":
         time.sleep(2)
         driver.find_element(By.CSS_SELECTOR, "#passwordNext > div > button > span").click()
         time.sleep(5)
-        print("LOGIN COMPLETED")
-    def generate_link(driver, current_row):
+        print("--LOGIN COMPLETED--")
+    def generate_link(driver, sheet, current_row):
         driver.find_element(By.CSS_SELECTOR,
                             "#yDmH0d > c-wiz > div > div.S3RDod > div > div.Qcuypc > div.Ez8Iud > div > div.VfPpkd-xl07Ob-XxIAqe-OWXEXe-oYxtQd > div:nth-child(1) > div > button > span").click()
         time.sleep(1)
@@ -62,19 +68,21 @@ if __name__=="__main__":
         link = driver.find_element(By.CSS_SELECTOR,
                                    "#yDmH0d > div.VfPpkd-Sx9Kwc.VfPpkd-Sx9Kwc-OWXEXe-vOE8Lb.cC1eCc.UDxLd.PzCPDd.VKf0Le.u9lF8e.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div.VfPpkd-cnG4Wd > div > div:nth-child(2) > div > div.NgL38b.CZ8zsc > div.VA2JSc")
         print(link.text)
-        originsheet.cell(row=current_row, column=14).value = link.text
+        sheet.cell(row=current_row, column=14).value = link.text
         driver.find_element(By.CSS_SELECTOR,
                             "div.u9lF8e div.VfPpkd-oclYLd > button > span > svg").click()
         time.sleep(1)
 
     print("START!")
+    prepare_form(originsheet)
+
     driver = uc.Chrome()
     driver.get('https://meet.google.com/')
     driver.maximize_window()
     time.sleep(2)
     login(driver) #로그인
     for i in range(0, MAX_ROW):
-        generate_link(driver, i+2)
+        generate_link(driver, originsheet, i+2)
 
     print("END!")
 

b17b2616672a0c98eacfe5f5736d2f6f40b95f20
ottl-seo <dotsi@ewhain.net>
2022-11-13 02:41:42 +0900

    [#14][#3] 셀에 링크 붙여넣기 완료

--- driver.py
+++ driver.py
@@ -6,13 +6,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import undetected_chromedriver as uc
 import accountInfo
-"""
+
 ###### [STEP 1] 수험자 리스트 엑셀 열기 ######
 wb = openpyxl.load_workbook("./data/자격시험수험자리스트_1102.xlsx")
 originsheet = wb.active
 
 MAX_ROW = -1
-
+"""
 ###### [STEP 2] 기본 정보 붙여넣기 ######
 # 이메일 셀 범위 복사 붙여넣기 (C열-> L열로 복사)
 columns = originsheet.iter_cols(min_col=3, max_col=3)
@@ -35,9 +35,15 @@ for i in range(0,MAX_ROW+2):
 """
 if __name__=="__main__":
     def login(driver):
+        ### 초기 1회 ###
+        # '로그인' 클릭: a.glue-header__link
+        # 아이디 입력: input#identifierId
+        # '다음' 클릭: #identifierNext > div > button > span
+        # 비밀번호 입력: #password > div.aCsJod.oJeWuf > div > div.Xb9hP > input
+        # '다음' 클릭: #passwordNext > div > button > span
+
         driver.find_element(By.CSS_SELECTOR, "a.glue-header__link").click()
         time.sleep(2)
-
         driver.find_element(By.CSS_SELECTOR, "input#identifierId").send_keys(accountInfo.admin_id)
         time.sleep(2)
         driver.find_element(By.CSS_SELECTOR, "#identifierNext > div > button > span").click()
@@ -52,19 +58,29 @@ if __name__=="__main__":
     driver = uc.Chrome()
     driver.get('https://meet.google.com/')
     driver.maximize_window()
-
     time.sleep(2)
-    login(driver)
+    login(driver) #로그인
+
+    driver.find_element(By.CSS_SELECTOR,
+                        "#yDmH0d > c-wiz > div > div.S3RDod > div > div.Qcuypc > div.Ez8Iud > div > div.VfPpkd-xl07Ob-XxIAqe-OWXEXe-oYxtQd > div:nth-child(1) > div > button > span").click()
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR,
+                        "#yDmH0d > c-wiz > div > div.S3RDod > div > div.Qcuypc > div.Ez8Iud > div > div.VfPpkd-xl07Ob-XxIAqe-OWXEXe-oYxtQd > div:nth-child(2) > div > ul > li:nth-child(2) > span.VfPpkd-StrnGf-rymPhb-b9t22c").click()
+    time.sleep(1)
+    #driver.find_element(By.CSS_SELECTOR, "div.VKf0Le.u9lF8e div:nth-child(2) > span > button").click()
+    #time.sleep(1)
+    link = driver.find_element(By.CSS_SELECTOR,
+                               "#yDmH0d > div.VfPpkd-Sx9Kwc.VfPpkd-Sx9Kwc-OWXEXe-vOE8Lb.cC1eCc.UDxLd.PzCPDd.VKf0Le.u9lF8e.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div.VfPpkd-cnG4Wd > div > div:nth-child(2) > div > div.NgL38b.CZ8zsc > div.VA2JSc")
+    print(link.text)
+    originsheet.cell(row=2, column=14).value = link.text
+    driver.find_element(By.CSS_SELECTOR,
+                        "div.u9lF8e div.VfPpkd-oclYLd > button > span > svg").click()
+    time.sleep(1)
     print("END!")
 
 ###### [STEP 3] Selenium으로 브라우저 원격 제어 ######
 
-        ### 초기 1회 ###
-    # '로그인' 클릭: a.glue-header__link
-    # 아이디 입력: input#identifierId
-    # '다음' 클릭: #identifierNext > div > button > span
-    # 비밀번호 입력: #password > div.aCsJod.oJeWuf > div > div.Xb9hP > input
-    # '다음' 클릭: #passwordNext > div > button > span
+
         ### 반복 ###
     # '새 회의' 버튼: div.VfPpkd-xl07Ob-XxIAqe-OWXEXe-oYxtQd > div:nth-child(1) > div > button > div.VfPpkd-RLmnJb
     # '나중에 진행할 회의 만들기' 버튼: c-wiz li:nth-child(2) > span.VfPpkd-StrnGf-rymPhb-b9t22c
@@ -74,7 +90,7 @@ if __name__=="__main__":
 # step 4. 엑셀에 정보 붙여넣기
 #sheet.append([title, genre, audience])
 
-#wb.save("./result/temp_1102.xlsx")
+wb.save("./result/temp_1102.xlsx")
 
 """
 # 나중에) 메일 전송용 엑셀 만들기

97349578abcc7661341a88595bf227e63f2d5c0d
ottl-seo <dotsi@ewhain.net>
2022-12-03 22:37:41 +0900

    [#23][#17] mailsheet, tempsheet 분리해서 중복 체크

--- driver.py
+++ driver.py
@@ -10,28 +10,34 @@ import undetected_chromedriver as uc
 import accountInfo
 
 def prepare_form(sheet):
-    global MAX_ROW
-    MAX_ROW = -1
+    global NUM_OF_ROWS
+    NUM_OF_ROWS = -1
 
     # 이메일 셀 범위 복사 붙여넣기 (C열-> L열로 복사)
     columns = originsheet.iter_cols(min_col=3, max_col=3)
     for col in columns:
         for cell in col:
             cell_new = originsheet.cell(row=cell.row, column=12, value=cell.value)
-            MAX_ROW += 1  # 총 row 개수는 (MAX_ROW-1) 개
+            NUM_OF_ROWS += 1  # 총 인원 수는 (NUM_OF_ROWS-1) 명
 
     # 참조용 메일 주소 넣기
-    originsheet.cell(row=MAX_ROW + 2, column=12).value = "dl_edu_cert@navercorp.com"
-    originsheet.cell(row=MAX_ROW + 3, column=12).value = "dl_certification@navercorp.com"
+    originsheet.cell(row=NUM_OF_ROWS + 2, column=12).value = "dl_edu_cert@navercorp.com"
+    originsheet.cell(row=NUM_OF_ROWS + 3, column=12).value = "dl_certification@navercorp.com"
 
     # 날짜 삽입 (A열-> M열)
     dateValue = (originsheet.cell(row=2, column=1).value) % 10000  ## 1102
     month = (int)(dateValue / 100)
     day = (dateValue % 100)
     dateString = str(month) + "월 " + str(day) + "일"  # 월-일 형태의 스트링으로 변환
-    for i in range(0, MAX_ROW + 2):
-        originsheet.cell(row=i + 2, column=13).value = dateString  # M열에 삽입
+    for i in range(2, NUM_OF_ROWS + 4):
+        originsheet.cell(row=i, column=13).value = dateString  # M열에 삽입
     print("--FORM GENERATED--")
+
+def set_mailer_form(sheet, mailsheet):
+    # 1) L,M,N열을 (2, MAX_ROW + 4) 만큼 복사 -> mailsheet의 A,B,C열 (2, MAX_ROW + 4)
+    # 2) drop_duplicate
+    global NUM_OF_PEOPLE
+    # NUM_OF_PEOPLE =
 
 def login(driver):  ### 초기 1회 ###
     # '로그인' 클릭
@@ -68,7 +74,7 @@ def login(driver):  ### 초기 1회 ###
     except: #NoSuchElementException
         print("--Authentification Success--")
     print("--LOGIN COMPLETED--")
-def generate_link(driver, sheet, current_row):
+def generate_link(driver, mailsheet, current_row):
     # '새 회의' 버튼
     driver.find_element(By.CSS_SELECTOR,
                         "#yDmH0d > c-wiz > div > div.S3RDod > div > div.Qcuypc > div.Ez8Iud > div > div.VfPpkd-xl07Ob-XxIAqe-OWXEXe-oYxtQd > div:nth-child(1) > div > button > span").click()
@@ -85,29 +91,47 @@ def generate_link(driver, sheet, current_row):
         link = driver.find_element(By.CSS_SELECTOR, "#yDmH0d > div.VfPpkd-Sx9Kwc.VfPpkd-Sx9Kwc-OWXEXe-vOE8Lb.cC1eCc.UDxLd.PzCPDd.VKf0Le.u9lF8e.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div.VfPpkd-cnG4Wd > div > div:nth-child(2) > div > div.Hayy8b")
         #print("USING LATEST CHROME VERSION..")
     print(link.text)
+
     # 셀에 삽입
-    sheet.cell(row=current_row, column=14).value = link.text
+    #sheet.cell(row=current_row, column=14).value = link.text
+    mailsheet.cell(row=current_row, column=3).value = link.text
     # 'X' 버튼
     driver.find_element(By.CSS_SELECTOR,
                         "div.u9lF8e div.VfPpkd-oclYLd > button > span > svg").click()
     time.sleep(1)
 
+def set_result_form(mailsheet, tempsheet):
+    # 1)
+    # mailsheet에서 (i,1)값을 가져와 resultsheet에서 찾는다
+    # resultsheet에 있는 값만큼
+    # NUM_OF_PEOPLE 만큼 반복
+    return tempsheet
 
 if __name__=="__main__":
-
+    # 초기 시트
     wb = openpyxl.load_workbook("./data/23일.xlsx")
     originsheet = wb.active
+    # 메일 전송용 시트
+    wb_mail = openpyxl.load_workbook("./mailer/MASS_INPUT_FORM.xlsx")
+    mailsheet = wb_mail.active
+    # 중복 체크용 임시 시트
+    wb_temp = openpyxl.load_workbook("./mailer/MASS_INPUT_FORM.xlsx")
+    tempsheet = wb_mail.active
 
     print("START!")
     prepare_form(originsheet)
+    set_mailer_form(originsheet, mailsheet)
 
     driver = uc.Chrome()
     driver.get('https://meet.google.com/')
     driver.maximize_window()
     time.sleep(2)
     login(driver) #로그인
-    for i in range(0, MAX_ROW):
-        generate_link(driver, originsheet, i+2)
+    for i in range(2, NUM_OF_PEOPLE+2):
+        generate_link(driver, mailsheet, i)
+
+    # mailersheet 생성이 완료되었다면 중복응시자를 같은값으로 체크하여 tempsheet에 링크를 넣는다
+    set_result_form(mailsheet, tempsheet)
 
     print("END!")
     wb.save("./result/temp_1123.xlsx")
